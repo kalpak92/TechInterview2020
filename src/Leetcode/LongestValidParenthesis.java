4533dbc7b11dc2cd9443f815f320fc42cba4c9da
Kalpak <kals9seals@gmail.com>
2021-01-07 01:58:58 -0500

    LongestValidParenthesis

---
+++ src/Leetcode/LongestValidParenthesis.java
@@ -0,0 +1,66 @@
+package Leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author kalpak
+ *
+ * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+ *
+ * Example 1:
+ *
+ * Input: s = "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()".
+ *
+ * Example 2:
+ *
+ * Input: s = ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()".
+ *
+ * Example 3:
+ *
+ * Input: s = ""
+ * Output: 0
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= s.length <= 3 * 104
+ * s[i] is '(', or ')'.
+ */
+
+public class LongestValidParenthesis {
+    public static int longestValidParentheses(String s) {
+        int result = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        // You can imagine there is a ) at index -1 position
+        // i.e. s[-1] = ')'
+        stack.push(-1);
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(')
+                stack.push(i);
+            else {
+                // if there is a match (i.e. "()"), then we can pop the stack
+                // and update the max length by (i - indices.top())
+                // (because the string in (indices.top(), i] is a valid parenthese)
+                // else if there isn't a match (i.e. "))"), then we can substitute
+                // the top of the stack by the latest one
+                stack.pop();
+                if(stack.isEmpty())
+                    stack.push(i);  // if the stack is empty, it means a "))" occurs
+                else
+                    result = Math.max(result, i - stack.peek());    // "()" occurs
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String str = ")()())";
+        System.out.println(longestValidParentheses(str));
+    }
+}
