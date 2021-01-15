d843bdb27a81d3e9a571e2813c9d09874b5daac3
Kalpak <kals9seals@gmail.com>
2021-01-15 00:40:04 -0500

    RotateArray

--- src/Leetcode/RotateArray.java
+++ src/Leetcode/RotateArray.java
@@ -56,6 +56,16 @@ public class RotateArray {
         }
     }
 
+    // Time : O(n); Space : O(n)
+    public void rotateNaive(int[] nums, int k) {
+        int[] temp = new int[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            temp[(i + k)%nums.length] = nums[i];
+
+        for(int i = 0; i < nums.length; i++)
+            nums[i] = temp[i];
+    }
+
     public static void printArray(int[] nums) {
         for(int i : nums)
             System.out.print(i + " ");
