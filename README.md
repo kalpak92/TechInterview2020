92157a6df6d9d67e3a89a136e1cd45ecccfd2bb9
Kalpak <kals9seals@gmail.com>
2021-02-06 18:51:09 -0500

    SameTree

--- README.md
+++ README.md
@@ -288,7 +288,8 @@ This repository contains the solutions to problems done from various resources f
 - [x] [Sum of Path as Numbers - Leetcode](https://github.com/kalpak92/TechInterview2020/blob/master/src/Leetcode/SumOfPathNumbers.java)
 - [x] [Maximum Path Sum - Leetcode](https://github.com/kalpak92/TechInterview2020/blob/master/src/Leetcode/MaximumPathSumBinaryTree.java)
 - [x] [Diameter of a Binary Tree - Leetcode](https://github.com/kalpak92/TechInterview2020/blob/master/src/Leetcode/DiameterOfATree.java)
-- [x] [Boundary of a Binary Tree - Leetcode]()
+- [x] [Boundary of a Binary Tree - Leetcode](https://github.com/kalpak92/TechInterview2020/blob/master/src/Leetcode/BoundaryOfBinaryTree.java)
+- [x] [Check of Same Tree - Leetcode]()
 
 ## Subsets and Backtracking
 - [x] [Subsets of all given unique elements - Leetcode](https://github.com/kalpak92/TechInterview2020/blob/master/src/Leetcode/SubsetsOfUniqueElements.java)
---
+++ src/Leetcode/SameTree.java
@@ -0,0 +1,53 @@
+package Leetcode;
+
+/**
+ * @author kalpak
+ *
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+ *
+ * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+ *
+ * Example 1:
+ * Input: p = [1,2,3], q = [1,2,3]
+ * Output: true
+ *
+ * Example 2:
+ * Input: p = [1,2], q = [1,null,2]
+ * Output: false
+ *
+ * Example 3:
+ * Input: p = [1,2,1], q = [1,1,2]
+ * Output: false
+ *
+ * Constraints:
+ *
+ * The number of nodes in both trees is in the range [0, 100].
+ * -10^4 <= Node.val <= 10^4
+ */
+
+public class SameTree {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null)
+            return true;
+
+        if(p == null || q == null)
+            return false;
+
+        if(p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(3);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(3);
+
+        System.out.println(isSameTree(root1, root2));
+    }
+}
