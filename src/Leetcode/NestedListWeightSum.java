473d55035e7b169e46f00f8dca6fde15578b826d
Kalpak <kals9seals@gmail.com>
2021-02-18 01:06:40 -0500

    NestedListWeightSum

---
+++ src/Leetcode/NestedListWeightSum.java
@@ -0,0 +1,79 @@
+package Leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author kalpak
+ *
+ * You are given a nested list of integers nestedList.
+ * Each element is either an integer or a list whose elements may also be integers or other lists.
+ *
+ * The depth of an integer is the number of lists that it is inside of.
+ * For example, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth.
+ *
+ * Return the sum of each integer in nestedList multiplied by its depth.
+ *
+ * Example 1:
+ * Input: nestedList = [[1,1],2,[1,1]]
+ * Output: 10
+ * Explanation: Four 1's at depth 2, one 2 at depth 1. 1*2 + 1*2 + 2*1 + 1*2 + 1*2 = 10.
+ *
+ *
+ * Example 2:
+ * Input: nestedList = [1,[4,[6]]]
+ * Output: 27
+ * Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3. 1*1 + 4*2 + 6*3 = 27.
+ *
+ *
+ * Example 3:
+ * Input: nestedList = [0]
+ * Output: 0
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nestedList.length <= 50
+ * The values of the integers in the nested list is in the range [-100, 100].
+ * The maximum depth of any integer is less than or equal to 50.
+ */
+public class NestedListWeightSum {
+    public interface NestedInteger {
+        // @return true if this NestedInteger holds a single integer, rather than a nested list.
+        boolean isInteger();
+        // @return the single integer that this NestedInteger holds, if it holds a single integer
+        // Return null if this NestedInteger holds a nested list
+        Integer getInteger();
+        // Set this NestedInteger to hold a single integer.
+        void setInteger(int value);
+        // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+        void add(NestedInteger ni);
+        // @return the nested list that this NestedInteger holds, if it holds a nested list
+        // Return empty list if this NestedInteger holds a single integer
+        List<NestedInteger> getList();
+    };
+
+    public int depthSum(List<NestedInteger> nestedList) {
+        Queue<NestedInteger> queue = new LinkedList<>();
+        queue.addAll(nestedList);
+        int result = 0;
+        int depth = 1;
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+
+            while(size-- > 0) {
+                NestedInteger current = queue.poll();
+
+                if(current.isInteger()) {
+                    result += current.getInteger() * depth;
+                } else {
+                    queue.addAll(current.getList());
+                }
+            }
+            depth++;
+        }
+        return result;
+    }
+}
