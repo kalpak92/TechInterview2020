0d5ba046bf15f96643876d3ac536b71c4bcf7d0b
Kalpak <kals9seals@gmail.com>
2021-03-01 02:15:13 -0500

    FirstUniqueCharacter

--- src/Leetcode/FirstUniqueCharacter.java
+++ src/Leetcode/FirstUniqueCharacter.java
@@ -1,6 +1,6 @@
 package Leetcode;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -23,20 +23,45 @@ import java.util.Map;
 
 public class FirstUniqueCharacter {
     public static int firstUniqChar(String s) {
-        Map<Character, Integer> characterFrequency = new LinkedHashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
 
         for(int i = 0; i < s.length(); i++) {
-            if(!characterFrequency.containsKey(s.charAt(i)))
-                characterFrequency.put(s.charAt(i), i);
-            else
-                characterFrequency.put(s.charAt(i), -1);
-        }
-
-        for(Character c : characterFrequency.keySet()) {
-            if(characterFrequency.get(c) != -1)
-                return characterFrequency.get(c);
+            if(map.get(s.charAt(i)) == 1)
+                return i;
         }
         return -1;
+    }
+
+    public static int firstUniqCharUsingTwoPointers(String s) {
+
+        // Use a slow pointer to point to the current unique character
+        // A fast pointer to scan the string.
+        // The fast pointer not only just add the count of the character. Meanwhile, when fast pointer finds the identical character of the character at the current slow pointer, we move the slow pointer to the next unique character or not visited character.
+
+        if(s == null || s.length() == 0)
+            return -1;
+
+        int slow = 0;
+        int fast = 0;
+        char[] chs = s.toCharArray();
+        int[] count=new int[256];
+
+        int n = chs.length;
+        while (fast < n) {
+            count[chs[fast]]++;
+            while (slow < n && count[chs[slow]] > 1){
+                slow++;
+            }
+
+            if (slow == n)
+                return -1;
+
+            fast++;
+        }
+        return slow;
     }
 
     public static void main(String[] args) {
