6b08fa74a2385745c156e84f1f4595499ce44282
Kalpak <kals9seals@gmail.com>
2021-03-02 22:29:37 -0500

    RemoveAllAdjacentDuplicatedInStringII

--- src/Leetcode/RemoveAllAdjacentDuplicatedInStringII.java
+++ src/Leetcode/RemoveAllAdjacentDuplicatedInStringII.java
@@ -38,6 +38,24 @@ import java.util.Deque;
  */
 
 public class RemoveAllAdjacentDuplicatedInStringII {
+    public static String removeDuplicatesMemoize(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        int[] count = new int[s.length()];
+
+        for(int i = 0; i < sb.length(); i++) {
+            if(i == 0 || sb.charAt(i) != sb.charAt(i - 1))
+                count[i] = 1;
+            else {
+                count[i] = count[i - 1] + 1;
+                if(count[i] == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    i = i - k;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
     public static String removeDuplicates(String s, int k) {
         Deque<Pair> stack = new ArrayDeque<>();
 
