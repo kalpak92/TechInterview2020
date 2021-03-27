12a206f004f10a725b7a98d0278c888d46d1653a
Kalpak <kals9seals@gmail.com>
2021-03-27 18:00:23 -0400

    RemoveKDigitsToGetSmallestNumber

---
+++ src/Leetcode/RemoveKDigitsToGetSmallestNumber.java
@@ -0,0 +1,66 @@
+package Leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author kalpak
+ *
+ * Given a non-negative integer num represented as a string,
+ * remove k digits from the number so that the new number is the smallest possible.
+ *
+ * Note:
+ * The length of num is less than 10002 and will be ≥ k.
+ * The given num does not contain any leading zero.
+ *
+ * Example 1:
+ * Input: num = "1432219", k = 3
+ * Output: "1219"
+ * Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+ *
+ * Example 2:
+ * Input: num = "10200", k = 1
+ * Output: "200"
+ * Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+ *
+ * Example 3:
+ * Input: num = "10", k = 2
+ * Output: "0"
+ * Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+ *
+ */
+
+public class RemoveKDigitsToGetSmallestNumber {
+    public static String removeKdigits(String num, int k) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char digit : num.toCharArray()) {
+            while(stack.size() > 0 && k > 0 && stack.peekLast() > digit) {
+                stack.removeLast();
+                k -= 1;
+            }
+
+            stack.addLast(digit);
+        }
+
+        while(k-- > 0)
+            stack.removeLast();
+
+        StringBuilder result = new StringBuilder();
+        boolean leadingZero = true;
+
+        for(char digit : stack) {
+            if(leadingZero && digit == '0')
+                continue;
+
+            leadingZero = false;
+            result.append(digit);
+        }
+
+        return (result.length() == 0) ? "0" : result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The smallest possible number after K removals is : " + removeKdigits("1432219", 3));
+    }
+}
